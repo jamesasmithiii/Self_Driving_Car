ab8c21854abf93324bba733522d14a1fa4e2b11f
jamesasmithiii <james.a.smith.iii@gmail.com>
2024-04-09 08:53:32 -0400

    Graphical car enhancements.

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const N = 100;
 const cars = generateCars(N);
 let bestCar = cars[0];
 if (localStorage.getItem("bestBrain")) {
- for (let i = 0; i < cars.length; i++) {
+  for (let i = 0; i < cars.length; i++) {
     cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
     if (i != 0) {
       NeuralNetwork.mutate(cars[i].brain, 0.1);
@@ -21,19 +21,18 @@ if (localStorage.getItem("bestBrain")) {
 }
 
 const traffic = [
-  new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
-  new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
-  new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2),
-  new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2),
-  new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2),
-  new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2),
-  new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2, getRandomColor()),
 ];
 
 animate();
 
 function save() {
-  console.log("Saving best brain...");
   localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
 }
 
@@ -43,7 +42,7 @@ function discard() {
 
 function generateCars(N) {
   const cars = [];
-  for (let i = 1; i < N; i++) {
+  for (let i = 1; i <= N; i++) {
     cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI"));
   }
   return cars;
@@ -57,7 +56,7 @@ function animate(time) {
     cars[i].update(road.borders, traffic);
   }
 
-  const bestCar = cars.find((c) => c.y == Math.min(...cars.map((c) => c.y)));
+  bestCar = cars.find((c) => c.y == Math.min(...cars.map((c) => c.y)));
 
   carCanvas.height = window.innerHeight;
   networkCanvas.height = window.innerHeight;
@@ -67,15 +66,15 @@ function animate(time) {
 
   road.draw(carCtx);
   for (let i = 0; i < traffic.length; i++) {
-    traffic[i].draw(carCtx, "red");
+    traffic[i].draw(carCtx);
   }
 
-  carCtx.globalAlpha = 1;
-  bestCar.draw(carCtx, "blue", true);
-
+  carCtx.globalAlpha = 0.2;
   for (let i = 0; i < cars.length; i++) {
-    cars[i].draw(carCtx, "blue");
+    cars[i].draw(carCtx);
   }
+  carCtx.globalAlpha = 1;
+  bestCar.draw(carCtx, true);
 
   carCtx.restore();
 
--- network.js
+++ network.js
@@ -1,8 +1,8 @@
 class NeuralNetwork {
-  constructor(neuralCounts) {
+  constructor(neuronCounts) {
     this.levels = [];
-    for (let i = 0; i < neuralCounts.length - 1; i++) {
-      this.levels.push(new Level(neuralCounts[i], neuralCounts[i + 1]));
+    for (let i = 0; i < neuronCounts.length - 1; i++) {
+      this.levels.push(new Level(neuronCounts[i], neuronCounts[i + 1]));
     }
   }
 
